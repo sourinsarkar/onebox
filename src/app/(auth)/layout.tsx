2f2408b2c528efe2d455d57e9fbeb42dc0ece49d
Sourin <iamsourinsarkar@gmail.com>
2024-07-18 03:31:36 +0530

    Done: Auth Layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
     return (
         <div className="relative h-screen bg-rib-1000">
             {/* Auth Navbar */}
-            <nav className="flex items-center justify-center">
+            <nav className="flex items-center justify-center h-16 border-b-[1px] border-rib-900">
                 <Image
                     src="/images/ribmark.svg"
                     width={156}
@@ -16,8 +16,8 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
             {children}
             
             {/* Auth Footer */}
-            <footer className="absolute w-full flex items-center justify-center bottom-0">
-                <p className="text-rib-700">&copy; 2023 Reachinbox. All rights reserved.</p>
+            <footer className="absolute w-full flex items-center justify-center bottom-0 h-8 border-t-[1px] border-rib-900">
+                <p className="text-rib-700 text-xs">&copy; 2023 Reachinbox. All rights reserved.</p>
             </footer>
         </div>
     );
