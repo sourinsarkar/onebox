c24509cef8c7f1ca1b74d32af3b6d5f1af78a0ea
Sourin <iamsourinsarkar@gmail.com>
2024-07-18 04:45:26 +0530

    Done: Priliminary Auth done

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="relative h-screen bg-rib-1000">
+        <div className="relative h-screen bg-black">
             {/* Auth Navbar */}
-            <nav className="flex items-center justify-center h-16 border-b-[1px] border-rib-900">
+            <nav className="flex items-center justify-center h-16 border-b-[1px] border-[#25262b]">
                 <Image
                     src="/images/ribmark.svg"
                     width={156}
@@ -13,10 +13,12 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
                 />
             </nav>
 
-            {children}
+            <div className="flex-1">
+                {children}
+            </div>
             
             {/* Auth Footer */}
-            <footer className="absolute w-full flex items-center justify-center bottom-0 h-8 border-t-[1px] border-rib-900">
+            <footer className="absolute w-full flex items-center justify-center bottom-0 h-8 border-t-[1px] border-[#25262b] bg-[#121212]">
                 <p className="text-rib-700 text-xs">&copy; 2023 Reachinbox. All rights reserved.</p>
             </footer>
         </div>
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,6 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Signup() {
     return (
-        <div>
+        <div className="h-screen flex items-center justify-center" style={{ height: `calc(100vh - 6rem)` }}>
+            <div className="m-auto w-[460px] border-[1px] border-[#343A40] rounded-2xl pt-6 px-10 pb-10 text-center space-y-10">
+                <section className="space-y-6">
+                    <h1 className="text-[#FFF] font-semibold text-xl">Create a new account</h1>
+                    <button className="w-full flex items-center justify-center gap-3 border-[1px] border-[#707172] rounded px-2 py-4 text-[#CCCCCC] leading-none">
+                        <Image
+                            src="/images/google_logo.png"
+                            width={16}
+                            height={16}
+                            alt="Reachinbox Icon"
+                        />
+                        <span>Sign Up with Google</span>
+                    </button>
+                </section>
+                <section className="space-y-6">
+                    <button className="text-white text-sm bg-[#4B63DD] py-3 px-8 rounded">
+                        Create an account
+                    </button>
+                    <p className="text-[#909296]">Already have an account? <Link href="#" className="text-[#C1C2C5]">Sign In</Link></p>
+                </section>
+            </div>
         </div>
     );
 }
