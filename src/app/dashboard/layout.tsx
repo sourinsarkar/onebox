3c5fb5082cf26a148d3cf04f394de68b82ee47d1
Sourin <iamsourinsarkar@gmail.com>
2024-07-19 07:40:18 +0530

    Fallback page done

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,6 +4,7 @@ import { Navbar } from "@/components/Nav/Navbar";
 import { SideNav } from "@/components/Nav/SideNav";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store/store";
+import Image from "next/image";
 
 export default function DashboardLayout({ children }: {children: React.ReactNode}) {
     const isOpen = useSelector((state: RootState) => state.inboxLayout.isOpen);
@@ -15,7 +16,20 @@ export default function DashboardLayout({ children }: {children: React.ReactNode
             </section>
             <section className="w-full">
                 <Navbar />
-                {isOpen ? children : <div className="p-4">This is a sample page</div>}
+                {isOpen ? children
+                :
+                <div className="flex items-center justify-center p-4 dark:bg-black" style={{ height: `calc(100vh - 4rem)` }}>
+                    <div className="flex flex-col justify-center items-center space-y-8">
+                        <Image
+                            src="/images/mailimage.svg"
+                            width={200}
+                            height={200}
+                            alt="Emails"
+                        />
+                        <h1 className="font-bold text-2xl text-center">It’s the beginning of a legendary sales pipeline </h1>
+                        <p className="font-medium text-lg text-center dark:text-[#9E9E9E]">When you have inbound E-mails you’ll see them here</p>                               
+                    </div>    
+                </div>}
             </section>
         </div>
     );
