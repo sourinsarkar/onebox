baa8579bed8ce81439771e3a4678ab75518a9ba0
Sourin <iamsourinsarkar@gmail.com>
2024-07-19 06:36:13 +0530

    Done: Dark version of editor

--- src/components/Mail/MailEditor.tsx
+++ src/components/Mail/MailEditor.tsx
@@ -30,33 +30,33 @@ export function MailEditor() {
     }
 
     return (
-        <div className="w-full mx-5 border-[1px] border-[#DEDEDE] bg-[#FAFAFA] py-3 px-4 rounded-lg">
+        <form className="w-full mx-5 border-[1px] border-[#DEDEDE] dark:border-[#4A5055] bg-[#FAFAFA] dark:bg-[#141517] py-3 px-4 rounded-lg">
             <section className="flex justify-between items-center">
                 <h2>Reply</h2>
                 <RiCloseFill  onClick={handleIsOpenClick} size={24} className="bg-[#d0a6a6] p-1 rounded-full"/>
             </section>
             <section>
-                <div className="py-3 border-b-[1px] border-[#DEDEDE] flex items-center">
-                    <p className="text-xs">To:</p>
-                    <input type="text" className="w-full bg-[#FAFAFA] outline-none" />
+                <div className="py-3 border-b-[1px] border-[#DEDEDE] dark:border-[#34383D] flex items-center gap-2">
+                    <label htmlFor="receiver" className="text-xs dark:text-[#BAB9BD]">To:</label>
+                    <input id="receiver" type="text" className="w-full bg-[#FAFAFA] dark:bg-[#141517] outline-none" />
                 </div>
-                <div className="py-3 border-b-[1px] border-[#DEDEDE] flex items-center">
-                    <p className="text-xs">From:</p>
-                    <input type="text" className="w-full bg-[#FAFAFA] outline-none" />
+                <div className="py-3 border-b-[1px] border-[#DEDEDE] dark:border-[#34383D] flex items-center gap-2">
+                    <label htmlFor="sender" className="text-xs dark:text-[#BAB9BD]">From:</label>
+                    <input type="text" id="sender" className="w-full bg-[#FAFAFA] dark:bg-[#141517] outline-none" />
                 </div>
-                <div className="py-3 border-b-[1px] border-[#DEDEDE] flex items-center">
-                    <p className="text-xs">Subject:</p>
-                    <input type="text" className="w-full bg-[#FAFAFA] outline-none" />
+                <div className="py-3 border-b-[1px] border-[#DEDEDE] dark:border-[#34383D] flex items-center gap-2">
+                    <label htmlFor="subject" className="text-xs dark:text-[#BAB9BD]">Subject:</label>
+                    <input type="text" id="subject" className="w-full bg-[#FAFAFA] dark:bg-[#141517] outline-none" />
                 </div>
             </section>
 
             <section>
-                <textarea className={`w-full bg-[#FAFAFA] outline-none h-64 p-4 ${isBold ? 'font-bold' : ''} ${isItalic ? 'italic' : 'not-italic'}`}
+                <textarea className={`w-full bg-[#FAFAFA] dark:bg-[#141517] outline-none h-64 p-4 ${isBold ? 'font-bold' : ''} ${isItalic ? 'italic' : 'not-italic'}`}
                     placeholder="Write your email here...">
                 </textarea>
             </section>
 
-            <section className="flex items-center gap-5">
+            <section className="flex items-center gap-5 border-[#DEDEDE] dark:border-[#2E3236]">
                 <button className="flex items-center justify-center gap-2 text-white font-semibold text-sm bg-[#4B63DD] py-3 px-4 rounded">
                     Send
                     <RiArrowDownSFill size={24} />
@@ -74,6 +74,6 @@ export function MailEditor() {
                     <RiItalic size={24}/>
                 </button>
             </section>
-        </div>
+        </form>
     );
 }
