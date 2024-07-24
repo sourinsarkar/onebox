54eee4a096c814d5677b95992df86c576e05702a
Sourin <iamsourinsarkar@gmail.com>
2024-07-24 17:34:48 +0530

    Signin page added

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -7,7 +7,7 @@ import { useSearchParams, useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
 import { setToken } from "@/redux/features/auth/authSlice";
 
-const SignupContent = () => {
+const SigninContent = () => {
     const router = useRouter();
     const dispatch = useDispatch();
     
@@ -30,7 +30,7 @@ const SignupContent = () => {
         <div className="h-screen flex items-center justify-center" style={{ height: `calc(100vh - 6rem)` }}>
             <div className="m-auto w-[460px] border-[1px] border-[#343A40] rounded-2xl pt-6 px-10 pb-10 text-center space-y-10">
                 <section className="space-y-6">
-                    <h1 className="text-[#FFF] font-semibold text-xl">Create a new account</h1>
+                    <h1 className="text-[#FFF] font-semibold text-xl">Sign into your account</h1>
                     <button onClick={handleLogin} className="w-full flex items-center justify-center gap-3 border-[1px] border-[#707172] rounded px-2 py-4 text-[#CCCCCC] leading-none">
                         <Image
                             src="/images/google_logo.png"
@@ -38,24 +38,24 @@ const SignupContent = () => {
                             height={16}
                             alt="Reachinbox Icon"
                         />
-                        <span>Sign Up with Google</span>
+                        <span>Sign In with Google</span>
                     </button>
                 </section>
                 <section className="space-y-6">
                     <button onClick={handleLogin} className="text-white font-semibold text-sm bg-[#4B63DD] py-3 px-8 rounded">
-                        Create an account
+                        Login
                     </button>
-                    <p className="text-[#909296]">Already have an account? <Link href="/signin" className="text-[#C1C2C5]">Sign In</Link></p>
+                    <p className="text-[#909296]">New here? <Link href="/signup" className="text-[#C1C2C5]">Create an account</Link></p>
                 </section>
             </div>
         </div>
     );
 }
 
-export default function Signup() {
+export default function Signin() {
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <SignupContent />
+            <SigninContent />
         </Suspense>
     );
 }
